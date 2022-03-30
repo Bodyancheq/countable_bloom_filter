290cb6e03dd709eefadfd43eb2ee51617d4a9cb7
Bogdan Abarikhin <bogdan_abarihin@mail.ru>
2022-03-30 17:37:18 +0400

    Заменил функциональный подход на ООП

--- bloom_filter.py
+++ bloom_filter.py
@@ -4,55 +4,55 @@ import random
 
 from text import text
 
-_memomask = {}
+
+class CBF:
+    def __init__(self, m: int, n=None, k=None):
+        """
+        :param m: Length of dictionary (len(set(words_list)))
+        :param n: Length of CBF
+        :param k: Number of hash functions
+        """
+        self._memomask = {}
+        self.m = m
+        self.n = self.m * 5 if n is None else n
+        self.k = int(self.n / self.m * math.log(2)) if k is None else k
+        self.hashes = [self.hash_function(i) for i in range(self.k)]
+        self.cbf = [0 for _ in range(self.n)]
+        self.fp_rate = round(pow(1 - math.exp(-self.k * self.m / self.n), self.k), 2)
+
+    def hash_function(self, n_):
+        mask = self._memomask.get(n_)
+        if mask is None:
+            random.seed(n_)
+            mask = self._memomask[n_] = random.getrandbits(32)
+
+        def myhash(x):
+            return hash(x) ^ mask
+
+        return myhash
+
+    def add_word(self, word):
+        indexes = [self.hashes[i](word) % self.n for i in range(self.k)]
+        for i in indexes:
+            self.cbf[i] += 1
+
+    def check_word(self, word):
+        check_indexes = [self.hashes[i](word) % self.n for i in range(self.k)]
+        if all([self.cbf[i] for i in check_indexes]):
+            print(f"Word [{word}] may be in text with probability of {1 - self.fp_rate}")
+        else:
+            print(f"No such word [{word}] in text")
 
 
-def hash_function(n_):
-    mask = _memomask.get(n_)
-    if mask is None:
-        random.seed(n_)
-        mask = _memomask[n_] = random.getrandbits(32)
+if __name__ == '__main__':
+    re_pattern = re.compile(r"[\w’]+")
+    words = re_pattern.findall(text)
+    dictionary = set(words)
 
-    def myhash(x):
-        return hash(x) ^ mask
+    my_cbf = CBF(m=len(dictionary))
+    for word in words:
+        my_cbf.add_word(word)
 
-    return myhash
-
-
-re_pattern = re.compile(r"[\w’]+")
-words = re_pattern.findall(text)
-dictionary = set(words)
-
-n = len(dictionary)
-print(f"m = {n}\nn = 5 * m")
-k = int((n * 5) / n * math.log(2))
-print(f"k = {k}")
-hashes = [hash_function(i) for i in range(k)]
-cbf = [0 for i in range(n * 5)]
-fp_rate = round(pow(1 - math.exp(-k * n / (5 * n)), k), 2)
-print(f"False positive rate is {fp_rate}")
-
-for word in dictionary:
-    indexes = [hashes[i](word) % len(cbf) for i in range(k)]
-    for i in indexes:
-        cbf[i] += 1
-
-
-def check_word(to_check):
-    check_indexes = [hashes[i](to_check) % len(cbf) for i in range(k)]
-    if all([cbf[i] for i in check_indexes]):
-        print(f"Word [{to_check}] may be in text")
-    else:
-        print(f"No such word [{to_check}] 100%")
-
-
-def add_word(new_word):
-    add_indexes = [hashes[i](new_word) % len(cbf) for i in range(k)]
-    for i in add_indexes:
-        cbf[i] += 1
-    print(f"Added word [{new_word}]")
-
-
-check_word("abobus")
-add_word("abobus")
-check_word("abobus")
+    my_cbf.check_word("aboba")
+    my_cbf.add_word("aboba")
+    my_cbf.check_word("aboba")
